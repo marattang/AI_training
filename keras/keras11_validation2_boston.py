52baabbb1c7b9e47a46e7bdc288645537276d9c5
marattang <tlawlfp0322@gmail.com>
2021-07-12 17:45:48 +0900

    2021-07-12

--- keras/keras11_validation2_boston.py
+++ keras/keras11_validation2_boston.py
@@ -1,9 +1,12 @@
+# 보스턴 주택가격을 완성할것.
+# 0.72
 # 과제3 loss, r2출력
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.metrics import r2_score
 from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
 
 datasets = load_boston()
 x = datasets.data
@@ -14,8 +17,10 @@ model.add(Dense(9, input_dim=13))
 model.add(Dense(8))
 model.add(Dense(1))
 
+x_train, x_ren, y_train, y_ren = train_test_split(x, y, train_size=0.6, random_state=66)
+
 model.compile(loss="mse", optimizer="adam", loss_weights=1)
-model.fit(x, y, epochs=500, batch_size=1)
+model.fit(x_train, y_train, epochs=25000, batch_size=242, validation_split=0.2)
 
 loss = model.evaluate(x, y)
 print('loss : ', loss)
--- keras/keras15_RMSE.py
+++ keras/keras15_RMSE.py
@@ -1,11 +1,17 @@
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.model_selection import train_test_split
 
 # 1.데이터
 x = np.array([1,2,3,4,5])
 y = np.array([1,2,4,3,5])
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y,
+         test_size=0.2, shuffle=True, random_state=66)
+
 
 # 2.모델
 model = Sequential()
@@ -16,7 +22,6 @@ model.add(Dense(7))
 model.add(Dense(7))
 model.add(Dense(3))
 model.add(Dense(1))
-
 
 
 # 3.컴파일
@@ -33,5 +38,10 @@ print('6의 예측 값 : ', y_predict)
 r2 = r2_score(y_test, y_predict)
 print('r2 score : ', r2)
 
+def RMSE(y_test, y_predict):
+    return np.sqrt(mean_squared_error(y_test, y_predict))
+rmse = RMSE(y_test, y_predict)
+
+print("rmse스코어 : ", rmse)
 # 과제 2
 # R2를 0.9 올려라!!!
